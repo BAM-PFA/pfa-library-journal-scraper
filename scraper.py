eff8e2271d12bd35e17428193adeccbfdc6a3f4d
mcampos-quinn <mcq@berkeley.edu>
2016-09-08 15:27:01 -0700

    Cleaned up script.

--- scraper.py
+++ scraper.py
@@ -1,81 +1,45 @@
-import requests, datetime, smtplib
+import requests, datetime, smtplib, collections
 from bs4 import BeautifulSoup
 
 # titles: Cinemascope, Sight & Sound, Film Comment, Film Quarterly,
-# Film History, Cineaste [cancelled? not included here] Journal of Film Preservation
+# Film History, Cineaste [cancelled? not included here], Journal of Film Preservation
 
-bigList = []
+sightRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tsight+and+sound/tsight+and+sound/1,2,13,B/crC1764132&FF=tsight+and+sound&8,,9")
+scopeRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tcinemascope/tcinemascope/1,3,3,B/crC1740108&FF=tcinemascope&1,1,")
+commentRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+comment/tfilm+comment/1,1,2,B/crC1742200&FF=tfilm+comment&1,,2")
+histRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+history/tfilm+history/1,4,4,B/crC1996133&FF=tfilm+history&1,1,")
+quartRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+quarterly/tfilm+quarterly/1,2,3,B/crC1601308&FF=tfilm+quarterly&1,,2")
+presRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tjournal+of+film+pres/tjournal+of+film+pres/1,1,1,B/crC2284190&FF=tjournal+of+film+pres&1,1,")
+
+dictItems = [("Sight and Sound: ",sightRaw),("CinemaScope: ",scopeRaw),("Film Comment: ",commentRaw),
+        ("Film History: ",histRaw), ("Film Quarterly: ",quartRaw), ("Journal of Film Preservation: ",presRaw)]
+orderedDict = collections.OrderedDict(dictItems)
+
+detailList = []
+outList = []
 today = datetime.date.today()
 readableToday = today.strftime("%B %d, %Y")
 
-sightRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tsight+and+sound/tsight+and+sound/1,2,13,B/crC1764132&FF=tsight+and+sound&8,,9")
-ss = sightRaw.content
-sightSoup = BeautifulSoup(ss, "lxml")
-sightIssues = sightSoup.find_all("td", "checkinArrived")
-sightList = []
-# the for loop looks in the last box on the "Latest received" page for each title
-for i in sightIssues[-1]:
-    lines = str(i)
-    sightList.append(lines.strip())
-    
-bigList.append("Sight and Sound: "+str(sightList[0])+" Volume "+str(sightList[8]))
+def scrape(title):
+    content = title.content
+    soup = BeautifulSoup(content, "lxml")
+    issues = soup.find_all("td", "checkinArrived")
+    issueList = []
+    for i in issues[-1]:
+        lines = str(i)
+        issueList.append(lines.strip())
+    whatIwant = str(issueList[0])+" Volume "+str(issueList[8])
+    detailList.append(whatIwant)
 
-scopeRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tcinemascope/tcinemascope/1,3,3,B/crC1740108&FF=tcinemascope&1,1,")
-cs = scopeRaw.content
-scopeSoup = BeautifulSoup(cs, "lxml")
-scopeIssues = scopeSoup.find_all("td", "checkinArrived")
-scopeList = []
-for i in scopeIssues[-1]:
-    lines = str(i)
-    scopeList.append(lines.strip())
-    
-bigList.append("Cinemascope: "+str(scopeList[0])+" Volume "+str(scopeList[8]))
+for title in orderedDict.keys():
+    scrape(orderedDict[title])
 
-commentRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+comment/tfilm+comment/1,1,2,B/crC1742200&FF=tfilm+comment&1,,2")
-fc = commentRaw.content
-commSoup = BeautifulSoup(fc, "lxml")
-commIssues = commSoup.find_all("td", "checkinArrived")
-commList = []
-for i in commIssues[-1]:
-    lines = str(i)
-    commList.append(lines.strip())
-    
-bigList.append("Film Comment: "+str(commList[0])+" Volume "+str(commList[8]))
+for a in detailList:
+    x = [value[1] for value in list(enumerate(orderedDict))]
+    y = str(x[detailList.index(a)])+a
+    outList.append(y)
 
-histRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+history/tfilm+history/1,4,4,B/crC1996133&FF=tfilm+history&1,1,")
-fh = histRaw.content
-histSoup = BeautifulSoup(fh, "lxml")
-histIssues = histSoup.find_all("td", "checkinArrived")
-histList = []
-for i in histIssues[-1]:
-    lines = str(i)
-    histList.append(lines.strip())
-    
-bigList.append("Film History: "+str(histList[0])+" Volume "+str(histList[8]))
-
-quartRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tfilm+quarterly/tfilm+quarterly/1,2,3,B/crC1601308&FF=tfilm+quarterly&1,,2")
-fq = quartRaw.content
-quartSoup = BeautifulSoup(fq, "lxml")
-quartIssues = quartSoup.find_all("td", "checkinArrived")
-quartList = []
-for i in quartIssues[-1]:
-    lines = str(i)
-    quartList.append(lines.strip())
-    
-bigList.append("Film Quarterly: "+str(quartList[0])+" Volume "+str(quartList[8]))
-
-presRaw = requests.get("http://oskicat.berkeley.edu/search~S43?/tjournal+of+film+pres/tjournal+of+film+pres/1,1,1,B/crC2284190&FF=tjournal+of+film+pres&1,1,")
-fp = presRaw.content
-presSoup = BeautifulSoup(fp, "lxml")
-presIssues = presSoup.find_all("td", "checkinArrived")
-presList = []
-for i in presIssues[-1]:
-    lines = str(i)
-    presList.append(lines.strip())
-    
-bigList.append("Journal of Film Preservation: "+str(presList[0])+" Volume "+str(presList[8]))
-
-output = ("As of "+str(readableToday)+" these are the latest issues of periodicals received in the library:"+"\n\n"+("\n".join(bigList)))
+output = ("As of "+str(readableToday)+" these are the latest issues of periodicals received in the library:"+"\n\n"+("\n".join(outList)))
 
 def sendemail(from_addr, to_addr_list, cc_addr_list,
               subject, message,
@@ -86,17 +50,17 @@ def sendemail(from_addr, to_addr_list, cc_addr_list,
     header += 'Cc: %s\n' % ','.join(cc_addr_list)
     header += 'Subject: %s\n' % subject
     message = header + message
- 
+
     server = smtplib.SMTP(smtpserver)
     server.starttls()
     server.login(login,password)
     problems = server.sendmail(from_addr, to_addr_list, message)
     server.quit()
 
-sendemail(from_addr    = "pfalibraryjournals@gmail.com", 
-          to_addr_list = ["mcq@berkeley.edu","etc@domain.com"],
-          cc_addr_list = ["more@domain.com"], 
-          subject      = "Latest Journals Received by PFA Library as of: "+(str(readableToday)), 
+sendemail(from_addr    = "pfalibraryjournals@gmail.com",
+          to_addr_list = ["NAME@COLLEGE.edu"],
+          cc_addr_list = ["MORENAME@COLLEGE.edu"],
+          subject      = "Latest Journals Received by PFA Library as of: "+(str(readableToday)),
           message      = output,
           login        = "pfalibraryjournals",
           password     = "PASSWORD")
